e996482f8650d2158c45dd9104ff2faac0bcf384
tiennd4tin <tiennv.epu@gmail.com>
2020-08-31 09:57:54 +0700

    iio

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -9,7 +9,7 @@ namespace WindowsFormsApp2
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application.77
         /// </summary>
         [STAThread]
         static void Main()
